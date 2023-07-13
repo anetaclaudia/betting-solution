d410ec619af31d36e21823836fb6ddf1590a1bfd
Aneta Claudia Marttila <aneta.marttila@gmail.com>
2023-07-13 21:14:44 +0300

    Initial commit

---
+++ src/main/java/com/testassignment/randomnumberbetting/RandomNumberBettingApplication.java
@@ -0,0 +1,13 @@
+package com.testassignment.randomnumberbetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumberBettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomNumberBettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testassignment/randomnumberbetting/RandomNumberBettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testassignment.randomnumberbetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberBettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
