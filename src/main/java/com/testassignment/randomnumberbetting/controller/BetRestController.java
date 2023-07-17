cba9a6f84866ab510b35d525bf5d7f6c78f6444b
Aneta Claudia Marttila <aneta.marttila@gmail.com>
2023-07-18 00:06:32 +0300

    Add REST controller for placing random number bet

---
+++ src/main/java/com/testassignment/randomnumberbetting/controller/BetRestController.java
@@ -0,0 +1,22 @@
+package com.testassignment.randomnumberbetting.controller;
+
+import com.testassignment.randomnumberbetting.dto.BetRequest;
+import com.testassignment.randomnumberbetting.dto.BetResponse;
+import com.testassignment.randomnumberbetting.service.RandomNumberBettingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BetRestController {
+
+  private final RandomNumberBettingService service;
+
+  @PostMapping("/bet")
+  public BetResponse betRandomNumber(@RequestBody @Valid BetRequest request) {
+    return service.betRandomNumber(request);
+  }
+}
