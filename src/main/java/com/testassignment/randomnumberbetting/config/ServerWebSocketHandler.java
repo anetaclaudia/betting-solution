f6f923b073b1247c66a4fabe9631c26183396195
Aneta Claudia Marttila <aneta.marttila@gmail.com>
2023-07-18 22:49:48 +0300

    Add validation to web socket handler.

--- src/main/java/com/testassignment/randomnumberbetting/config/ServerWebSocketHandler.java
+++ src/main/java/com/testassignment/randomnumberbetting/config/ServerWebSocketHandler.java
@@ -3,6 +3,9 @@ package com.testassignment.randomnumberbetting.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testassignment.randomnumberbetting.dto.BetRequest;
 import com.testassignment.randomnumberbetting.service.RandomNumberBettingService;
+import com.testassignment.randomnumberbetting.util.error.ErrorCode;
+import com.testassignment.randomnumberbetting.util.error.GeneralError;
+import java.math.BigDecimal;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -11,6 +14,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Service
 public class ServerWebSocketHandler extends TextWebSocketHandler {
 
+  private static final int MAX_RANDOM_NUMBER = 100;
+  private static final int MIN_RANDOM_NUMBER = 1;
   private final RandomNumberBettingService service = new RandomNumberBettingService();
 
   @Override
@@ -19,8 +24,24 @@ public class ServerWebSocketHandler extends TextWebSocketHandler {
     ObjectMapper objectMapper = new ObjectMapper();
     BetRequest betRequest = objectMapper.readValue(request, BetRequest.class);
 
-    session.sendMessage(
-        new TextMessage(objectMapper.writeValueAsString(service.betRandomNumber(betRequest)))
-    );
+    if (!isRequestValid(betRequest)) {
+      GeneralError errorResponse = new GeneralError(ErrorCode.INVALID_REQUEST,
+          "Random number must be in range 1-100. Bet must be at least 1.");
+
+      session.sendMessage(
+          new TextMessage(objectMapper.writeValueAsString(errorResponse))
+      );
+    } else {
+      session.sendMessage(
+          new TextMessage(objectMapper.writeValueAsString(service.betRandomNumber(betRequest)))
+      );
+    }
+  }
+
+  private boolean isRequestValid(BetRequest request) {
+    boolean isRandomNumberInRange = request.randomNumber() <= MAX_RANDOM_NUMBER
+        || request.randomNumber() >= MIN_RANDOM_NUMBER;
+    boolean isBetPositive = request.bet().compareTo(BigDecimal.ONE) > 0;
+    return isRandomNumberInRange && isBetPositive;
   }
 }
